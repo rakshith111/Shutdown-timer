a89214d3527a2b9d385f9eab024c98d7583b87be
rakshith111 <rakshithbabu111@gmail.com>
2021-10-24 18:51:28 +0530

    Resized windows and Changed timer mode

--- timer.py
+++ timer.py
@@ -5,12 +5,12 @@ def countdown(root,time_sec):
     hour=StringVar()
     minute=StringVar()
     second=StringVar()
-    root.minsize(225, 150)
-    root.maxsize(225, 150)
+    root.minsize(300, 160)
+    root.maxsize(300, 160)
     Hrt_label = tk.Label(root, text = 'Hrs', font=('calibre',10, 'bold'))
     Mint_label = tk.Label(root, text = 'Min', font=('calibre',10, 'bold'))
     Sect_label = tk.Label(root, text = 'Sec', font=('calibre',10, 'bold'))
-    Time_rem_label=tk.Label(root, text = 'Time remaining', font=('calibre',10, 'bold'))
+    Time_rem_label=tk.Label(root, text = 'Time \nremaining', font=('calibre',10, 'bold'))
     Time_rem_label.grid(row=5,column=0)
     Hrt_label.grid(row=6,column=0)
     Mint_label.grid(row=6,column=1)
@@ -23,16 +23,13 @@ def countdown(root,time_sec):
         hour.set("{0:2d}".format(hours))
         minute.set("{0:2d}".format(mins))
         second.set("{0:2d}".format(secs))
-        hrwindo= Entry(root, width=3, font=("Arial",18,""),
-                 textvariable=hour)
-        minwindo= Entry(root, width=3, font=("Arial",18,""),
-                   textvariable=minute)
-        secondswino= Entry(root, width=3, font=("Arial",18,""),
-                   textvariable=second)
+        hrwindo= Entry(root, width=3, font=('calibre',10, 'bold'),textvariable=hour)
+        minwindo= Entry(root, width=3, font=('calibre',10, 'bold'),textvariable=minute)
+        secondswino= Entry(root, width=3, font=('calibre',10, 'bold'),textvariable=second)
         hrwindo.grid(row=7,column=0)
         minwindo.grid(row=7,column=1)
         secondswino.grid(row=7,column=2)
  
+        root.after(1000)
         root.update()
-        time.sleep(1)
         time_sec-=1
